8aae91f0cabbc3ccac74f9064cc52ab2178b8ac0
user.email <pavithra.ramasamy2730@gmail.com>
2025-04-03 18:08:51 +0530

    Classes and ListOperationProgram

---
+++ PatternPrograms.py/ListOperations.py
@@ -0,0 +1,16 @@
+def listoperation(sampleList):
+    a=0
+    print("sampleList",sampleList)
+    sampleList.append(10) #Add a number to the list.
+    print("Addition", sampleList)
+    print("Remove",sampleList.pop()) #Remove a number from the list.
+    for x in sampleList: #Sum
+        a+=x
+    print("Sum",a) 
+    average = a//len(sampleList) #Average
+    print("average",average)
+    maximumValue = max(sampleList)
+    print("maximumValue", maximumValue)
+listoperation([1,2,3,4,5])
+
+
